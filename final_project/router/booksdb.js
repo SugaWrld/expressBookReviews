0b35fc6eba7cf7b9445c8c05a4abb7d92f05cb04
SugaWrld <nhkumcaleb@gmail.com>
2025-02-21 18:49:55 -0500

    All code completed

--- final_project/router/booksdb.js
+++ final_project/router/booksdb.js
@@ -1,14 +1,14 @@
 let books = {
-      1: {"author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {} },
-      2: {"author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {} },
-      3: {"author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {} },
-      4: {"author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {} },
-      5: {"author": "Unknown","title": "The Book Of Job", "reviews": {} },
-      6: {"author": "Unknown","title": "One Thousand and One Nights", "reviews": {} },
-      7: {"author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {} },
-      8: {"author": "Jane Austen","title": "Pride and Prejudice", "reviews": {} },
-      9: {"author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {} },
-      10: {"author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {} }
+      1: {"isbn": 1, "author": "Chinua Achebe","title": "Things Fall Apart", "reviews": {"reader_1": "Very Good!", "reader_2": "Great"} },
+      2: {"isbn": 2, "author": "Hans Christian Andersen","title": "Fairy tales", "reviews": {"reader_1": "Very Good!", "reader_2": "Great"} },
+      3: {"isbn": 3, "author": "Dante Alighieri","title": "The Divine Comedy", "reviews": {"reader_1": "Very Good!", "reader_2": "Great"} },
+      4: {"isbn": 4, "author": "Unknown","title": "The Epic Of Gilgamesh", "reviews": {"reader_1": "Very Good!", "reader_2": "Great"} },
+      5: {"isbn": 5, "author": "Unknown","title": "The Book Of Job", "reviews": {"reader_1": "Very Good!", "reader_2": "Great"} },
+      6: {"isbn": 6, "author": "Unknown","title": "One Thousand and One Nights", "reviews": {"reader_1": "Very Good!", "reader_2": "Great"} },
+      7: {"isbn": 7, "author": "Unknown","title": "Nj\u00e1l's Saga", "reviews": {"reader_1": "Very Good!", "reader_2": "Great"} },
+      8: {"isbn": 8, "author": "Jane Austen","title": "Pride and Prejudice", "reviews": {"reader_1": "Very Good!", "reader_2": "Great"} },
+      9: {"isbn": 9, "author": "Honor\u00e9 de Balzac","title": "Le P\u00e8re Goriot", "reviews": {"reader_1": "Very Good!", "reader_2": "Great"} },
+      10: {"isbn": 10, "author": "Samuel Beckett","title": "Molloy, Malone Dies, The Unnamable, the trilogy", "reviews": {"reader_1": "Very Good!", "reader_2": "Great"} }
 }
 
 module.exports=books;
